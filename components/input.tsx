8fd8fb33b28d3477902d24ca6ab28a394b47f19c
yeonjooU <yeonjoo@dable.io>
2023-03-13 22:16:41 +0900

    REFACTOR : react-hook-form 라이브러리로 form 수정

--- components/input.tsx
+++ components/input.tsx
@@ -1,15 +1,21 @@
+import type { UseFormRegisterReturn } from "react-hook-form";
+
 interface InputProps {
   label: string;
   name: string;
   kind?: "text" | "phone" | "price";
-  [key: string]: any;
+  type: string;
+  register: UseFormRegisterReturn;
+  required: boolean;
 }
 
 export default function Input({
   label,
   name,
   kind = "text",
-  ...rest
+  register,
+  type,
+  required,
 }: InputProps) {
   return (
     <div>
@@ -22,9 +28,11 @@ export default function Input({
       {kind === "text" ? (
         <div className="rounded-md relative flex  items-center shadow-sm">
           <input
-            id={name}
-            {...rest}
             className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            id={name}
+            type={type}
+            required={required}
+            {...register}
           />
         </div>
       ) : null}
@@ -34,9 +42,11 @@ export default function Input({
             <span className="text-gray-500 text-sm">$</span>
           </div>
           <input
-            id={name}
-            {...rest}
             className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            id={name}
+            type={type}
+            required={required}
+            {...register}
           />
           <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
             <span className="text-gray-500">KRW</span>
@@ -49,9 +59,11 @@ export default function Input({
             +82
           </span>
           <input
-            id={name}
-            {...rest}
             className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            id={name}
+            type={type}
+            required={required}
+            {...register}
           />
         </div>
       ) : null}
